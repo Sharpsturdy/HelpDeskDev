c526a0d478e9742109c0a1af90db0844d126431a
Andriy <andriy@progrex.co.uk>
2017-02-22 11:55:06 +0200

    show FAQ suggestions for standart users after suggestion approved

--- Controllers/FAQsController.cs
+++ Controllers/FAQsController.cs
@@ -292,10 +292,10 @@ namespace Help_Desk_2.Controllers
         {
             string userName = AllSorts.getUserID();
             var faqs = from m in db.KnowledgeFAQs
-                       where (m.type == 1 && m.suggest)
+                       where (m.type == 1 && m.originatorID.ToString()==userName)
                        select m;
 
-            faqs = faqs.Where(s => s.originatorID.ToString() == userName);
+            //faqs = faqs.Where(s => s.originatorID.ToString() == userName);
             
             int currentPageIndex = page.HasValue ? page.Value - 1 : 0;
             return View("Index", faqs.OrderByDescending(m => m.dateComposed).ToPagedList(currentPageIndex, AllSorts.pageSize));
