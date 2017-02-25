dde5bef234eb52b846907da804ff76a82c8310b9
Andriy <andriy@progrex.co.uk>
2017-02-25 19:01:23 +0200

    Fixed issue 063029

---
+++ Models/ReportTypeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Help_Desk_2.Models
+{
+    public enum ReportTypeEnum
+    {
+        Summary     = 1,
+        MonthToDate = 2,
+        YearToDate  = 3,
+        MonthToDateByUser = 4,
+        YearToDateByUser = 5,
+        OpenAssigned = 6
+    }
+}
